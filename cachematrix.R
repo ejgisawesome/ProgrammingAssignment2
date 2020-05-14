2188e6c85b5febb4242c0b2d6d73100b3764c6a1
ejgisawesome <egarcia4441@gmail.com>
2020-05-13 20:16:31 -0400

    Let's see if the first attempt works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An attempt to cache the inverse of a matrix so that
+## it's not necessary to recalculate.  Can't tell if it works.
 
-## Write a short comment describing this function
+## install and load matlib package to solve matrices
+install.packages("matlib")
+library(matlib)
+?inv #the function for inverse of a matrix, in matlib package
 
+## I guess this sets up the matrix and gets the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## hoping these work the same as example
+        ## we'll find out won't we?
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set,get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## If this works, it's going to calculate the inverse of the
+## matrix the last function made, but will skip if already
+## computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inv(data,...)
+        x$setinv(m)
+        m
 }
+
+## Let's see if the change to a matrix causes problems...
